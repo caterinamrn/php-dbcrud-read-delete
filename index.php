4336450b2270a40bdab4a530cef433396f5773a4
caterina <catemarian1806@gmail.com>
2020-09-14 17:02:12 +0200

    connessione al database

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
+  $servername ="localhost";
+  $user = "root";
+  $password = "nutella";
+  $dbname = "dbhotel";
+
+  $connection = new mysqli($servername,$user,$password,$dbname);
+  if ($connection && $connection-> connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+    return;
+  }
+  else {
+    echo "ok";
+  }
  ?>
