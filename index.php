fa4460db3cef09ddcd35833383011b96f1bb473e
caterina <catemarian1806@gmail.com>
2020-09-14 17:47:45 +0200

    select pagamenti

--- index.php
+++ index.php
@@ -6,10 +6,34 @@
 
   $connection = new mysqli($servername,$user,$password,$dbname);
   if ($connection && $connection-> connect_error) {
-    echo "Connection failed: " . $conn->connect_error;
+    echo "error: " . $conn->connect_error;
+    print_r( $connection );
+
     return;
   }
   else {
-    echo "ok";
+    echo "ok <br>";
   }
+
+  $sql = "
+          SELECT *
+          FROM pagamenti
+      ";
+
+  $result = $connection->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    echo "<ul>";
+      while ($row = $result->fetch_assoc()) {
+        echo "<li> prenotazione " . $row["id"] . " : " . $row["status"] . " - " . $row["price"] . "</li>";
+      }
+    echo "</ul>";
+  }
+  elseif ($result) {
+    echo "no result";
+  }
+  else {
+    echo "error";
+  }
+
  ?>
